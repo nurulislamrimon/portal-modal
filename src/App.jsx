f6760481549736260f187772b9bf937c8c73394b
nurulislamrimon <nurulislamrimon@gmail.com>
2024-05-30 12:38:57 +0600

    create portal separeted from each modal call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Modal from "./Modal";
-import { createPortal } from "react-dom";
 
 function App() {
   const [show, setShow] = React.useState(false);
@@ -11,10 +10,7 @@ function App() {
       <div>This is Portal Modal App</div>
       <button onClick={() => setShow(true)}>Open Modal</button>
 
-      {
-        // This is the Portal Modal App
-        show && createPortal(<Modal setShow={setShow} />, document.body)
-      }
+      {show && <Modal setShow={setShow} show={show} />}
     </section>
   );
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import { createPortal } from "react-dom";
 
 const Modal = ({ setShow }) => {
-  return (
+  // This is the Portal Modal App
+  return createPortal(
     <div className="modal">
       <h1>Hello! This is a Portal Modal</h1>
       <button onClick={() => setShow(false)}>Close</button>
-    </div>
+    </div>,
+    document.body
   );
 };
 
