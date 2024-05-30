a247e3a8a2835fdf5c2a36d915798206c934166b
nurulislamrimon <nurulislamrimon@gmail.com>
2024-05-30 12:31:26 +0600

    modal created with functionality

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,16 @@
-const Modal = (setShow) => {
+import PropTypes from "prop-types";
+
+const Modal = ({ setShow }) => {
   return (
     <div className="modal">
-      Hello
+      <h1>Hello! This is a Portal Modal</h1>
       <button onClick={() => setShow(false)}>Close</button>
     </div>
   );
+};
+
+Modal.propTypes = {
+  setShow: PropTypes.func.isRequired,
 };
 
 export default Modal;
